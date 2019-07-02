2c2f578fd4f48b65c75a1467f83d39e6fe3e4001
Alexander Guettler <alexander@guettler.io>
2019-07-02 12:36:09 -0700

    Add initial util functions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+# Meow Hook
 
+A simple hooking library I use in my mods, it has some support for detour with relocation, helper functions for various patching operations and a fast pattern search
+implementation
+
+> The current implementation is windows only
+
+> Documentation is coming as soon as I have finished the initial implementation and abstracted some windows specific things
---
+++ include/meow_hook/util.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+
+namespace meow_hook
+{
+template <typename R> static R __thiscall func_call(uint64_t addr)
+{
+    return ((R(*)())(addr))();
+}
+
+template <typename R, typename... Args> struct func_call_member_helper {
+    using func_t = R (func_call_member_helper::*)(Args...) const;
+
+    R operator()(func_t func_ptr, Args... args) const
+    {
+        return (this->*func_ptr)(args...);
+    }
+};
+
+template <typename R, class T, typename... Args>
+R func_call_member(uint64_t addr, T _this, Args... args)
+{
+    const func_call_member_helper<R, Args...> *helper =
+        reinterpret_cast<const func_call_member_helper<R, Args...> *>(_this);
+    auto           func_ptr = *(typename std::remove_pointer_t<decltype(helper)>::func_t *)(&addr);
+    return helper->operator()(func_ptr, args...);
+}
+
+template <typename R, class T, typename... Args>
+inline std::enable_if_t<!std::is_pointer_v<T> || !std::is_class_v<std::remove_pointer_t<T>>, R>
+func_call(uint64_t addr, T _this, Args... args)
+{
+    return ((R(*)(T, Args...))(addr))(_this, args...);
+}
+
+template <typename R, class T, typename... Args>
+inline std::enable_if_t<std::is_pointer_v<T> && std::is_class_v<std::remove_pointer_t<T>>, R>
+func_call(uint64_t addr, T _this, Args... args)
+{
+    return func_call_member<R, T, Args...>(addr, _this, args...);
+}
+} // namespace meow_hook
