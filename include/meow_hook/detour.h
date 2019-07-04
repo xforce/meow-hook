7f7195005fc4b2515ae247804a296b9fb7689dfd
Alexander Guettler <alexander@guettler.io>
2019-07-04 10:40:32 -0700

    Playing around with a helper macro

--- include/meow_hook/detour.h
+++ include/meow_hook/detour.h
@@ -74,4 +74,21 @@ namespace detail
 } // namespace detail
 
 template <typename T> using detour = detail::detour<T>;
+
+// From:
+// http://stackoverflow.com/questions/1082192/how-to-generate-random-variable-names-in-c-using-macros/17624752#17624752
+//
+#define MH_PP_CAT(a, b) MH_PP_CAT_I(a, b)
+#define MH_PP_CAT_I(a, b) MH_PP_CAT_II(~, a##b)
+#define MH_PP_CAT_II(p, res) res
+
+#define MH_STATIC_DETOOUR_IMPL(n, addr, fn)                                                        \
+    (([]() -> auto {                                                                               \
+        static ::meow_hook::detour<decltype(fn)> n{addr, fn};                                      \
+        return n.trampoline();                                                                     \
+    })())
+
+#define MH_STATIC_DETOUR(addr, fn)                                                                 \
+    MH_STATIC_DETOOUR_IMPL(MH_PP_CAT(mh_detour, __COUNTER__), addr, fn)
+
 } // namespace meow_hook
